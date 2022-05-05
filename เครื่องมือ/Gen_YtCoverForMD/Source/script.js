cb99a9c8897eba6e19036003c2cb0580cfd7563c
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-05 23:39:27 +0700

    Update script.js

--- เครื่องมือ/Gen_YtCoverForMD/Source/script.js
+++ เครื่องมือ/Gen_YtCoverForMD/Source/script.js
@@ -21,7 +21,7 @@ function Process(){
  }
  else {
   sum="[![ปกคลิปยูทูป](https://img.youtube.com/vi/"
-  sum+=yt_link_type[yt_link_type2.length-2]+")"
+  sum+=yt_link_type[yt_link_type2.length-1]+")"
   sum+="("+yt_link+")"
  }
  Outp=sum
