d8c12699b99a57314529e2718b3242346010dbb5
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-05 23:54:36 +0700

    Update script.js

--- เครื่องมือ/Gen_YtCoverForMD/Source/script.js
+++ เครื่องมือ/Gen_YtCoverForMD/Source/script.js
@@ -1,15 +1,13 @@
 function Process(){
  var yt_link=document.getElementById("Yt_link")
- yt_link=yt_link.value
  var Outp=document.getElementById("Output")
- Outp=Outp.value
 
  //Link type1 : https://www.youtube.com/watch?v=UUtZNjkFnlk
  //Link type2 : https://youtu.be/UUtZNjkFnlk
  //Cover link : https://img.youtube.com/vi/idytvdo
 
- var yt_link_type1=yt_link.split("v=")
- var yt_link_type2=yt_link.split("youtu.be/")
+ var yt_link_type1=yt_link.value.split("v=")
+ var yt_link_type2=yt_link.value.split("youtu.be/")
  var cover_link="https://img.youtube.com/vi/"
  var sum=""
 
@@ -24,7 +22,7 @@ function Process(){
   sum+=yt_link_type[yt_link_type2.length-1]+")"
   sum+="("+yt_link+")"
  }
- Outp=sum
+ Outp.value=sum
 }
 
 
