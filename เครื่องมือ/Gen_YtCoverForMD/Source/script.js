8a1a3505c720fa733804880d75086c4968ff421d
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-06 01:12:20 +0700

    Update script.js

--- เครื่องมือ/Gen_YtCoverForMD/Source/script.js
+++ เครื่องมือ/Gen_YtCoverForMD/Source/script.js
@@ -1,7 +1,8 @@
 Gen()
 function Gen(){
- var yt_link=document.getElementById("Yt_link")
- var Outp=document.getElementById("Output")
+ var yt_link = document.getElementById("Yt_link")
+ var Outp = document.getElementById("Output")
+ var yt_cover = document.getElementById("Yt_cover")
 
  //Link type1 : https://www.youtube.com/watch?v=UUtZNjkFnlk
  //Link type2 : https://youtu.be/UUtZNjkFnlk
@@ -9,7 +10,7 @@ function Gen(){
 
  var yt_link_type1=yt_link.value.split("v=")
  var yt_link_type2=yt_link.value.split("youtu.be/")
- //var cover_link="https://img.youtube.com/vi/"
+ var cover_link="https://img.youtube.com/vi/"
  var sum=""
 
  //[![ปกคลิปยูทูป](https://img.youtube.com/vi/idyt)(Linkyt)
@@ -17,13 +18,18 @@ function Gen(){
   sum="[![ปกคลิปยูทูป](https://img.youtube.com/vi/"
   sum+=yt_link_type1[yt_link_type1.length-1]+"/0.jpg)"
   sum+="("+yt_link.value+")"
+
+  cover_link+=yt_link_type1[yt_link_type1.length-1]+"/0.jpg"
  }
  else {
   sum="[![ปกคลิปยูทูป](https://img.youtube.com/vi/"
   sum+=yt_link_type2[yt_link_type2.length-1]+"/0.jpg)"
   sum+="("+yt_link.value+")"
+
+  cover_link+=yt_link_type2[yt_link_type2.length-1]+"/0.jpg"
  }
  Outp.innerText=sum
+ yt_cover.src=cover_link
 }
 
 
