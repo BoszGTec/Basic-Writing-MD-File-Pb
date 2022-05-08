89aea571b6c3fdf1e78df6d6d016f06c76f6dd23
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-08 22:09:46 +0700

    Update theme.js

--- เครื่องมือ/Gen_YtCoverForMD/Source/theme.js
+++ เครื่องมือ/Gen_YtCoverForMD/Source/theme.js
@@ -15,15 +15,19 @@ function c_theme() {
                       color : var(--bs-gray-300) ; "
   }
   
-  /*var theme = documents.getElementById("theme")
-  theme.innerHTML = '<h1>Gen_YtCoverForMD</h1> \
-                      <button class="btn btn-dark" onclick="c_theme_2()"> \
-                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb" viewBox="0 0 16 16"> \
-                        <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/> \
-                       </svg> \ 
-                      </button> '*/
-  
+  set_button()
 }
+
+function set_button(){  
+  var theme = documents.getElementById("theme")
+  theme.innerHTML = '<h1>Gen_YtCoverForMD</h1>'
+  theme.innerHTML += ' <button class="btn btn-dark" onclick="c_theme_2()"> '
+  theme.innerHTML += '  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb" viewBox="0 0 16 16"> '
+  theme.innerHTML += '   <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/> '
+  theme.innerHTML += '  </svg>' 
+  theme.innerHTML += ' </button>' 
+}
+
 
 function c_theme_2() {
   var body = document.getElementsByTagName("body")[0]
@@ -38,13 +42,15 @@ function c_theme_2() {
   for(let i=0;i<texta.length;i++){
     texta[i].style = ""
   }
-  
+  set_button_2()
+}  
+
+function set_button_2(){
   var theme = documents.getElementById("theme")
-  theme.innerHTML = ' <h1>Gen_YtCoverForMD</h1> \
-                      <button class="btn btn-dark" onclick="c_theme()"> \
-                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb" viewBox="0 0 16 16"> \
-                        <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/> \
-                       </svg> \
-                      </button>'
-  
+  theme.innerHTML = '<h1>Gen_YtCoverForMD</h1>'
+  theme.innerHTML += ' <button class="btn btn-dark" onclick="c_theme()">'
+  theme.innerHTML += '  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb" viewBox="0 0 16 16"> '
+  theme.innerHTML += '   <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/> '
+  theme.innerHTML += '  </svg>'                     
+  theme.innerHTML += ' </button>' 
 }
