8dc2ffb75af63328144dc70507ac657fe7f95ddc
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-08 20:48:38 +0700

    Create theme.js

---
+++ เครื่องมือ/Gen_YtCoverForMD/Source/theme.js
@@ -0,0 +1,7 @@
+function c_theme() {
+  var body=document.getElementsByTagName("body")[0]
+  body.style = "background-color : var(--bs-dark) ;"
+  body.style += "color : var(--bs-gray-300); "
+  
+  body = null 
+}
